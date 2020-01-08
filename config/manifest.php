6c26442fd67d54f4eca2e4421f4886269bd2194d
Sean O'Donnell <xengaming@outlook.com>
2020-01-08 22:34:52 +0000

    CSS Changes and such

--- config/manifest.php
+++ config/manifest.php
@@ -1,5 +1,5 @@
 <?php
-// auto-generated: 1578520360400
+// auto-generated: 1578522502337
 return [
 	"common.vendor.js" => "common.vendor.0e1e178705709c05235e.js",
 	"runtime.js" => "runtime.0c2e3a1b17584601ee44.js",
@@ -11,7 +11,7 @@ return [
 	"profile.js" => "profile.70da701bb149f4c29e1d.js",
 	"streamchat.js" => "streamchat.a87b6ad34c1a9dc3a14d.js",
 	"votechat.js" => "votechat.6d30683cf5107079b38e.js",
-	"web.css" => "web.be82eba73cf612af1d51.css",
+	"web.css" => "web.c5fd543292be7fb9eaed.css",
 	"web.js" => "web.a9177aaa55395a7d7349.js",
 	"font/style.scss" => "font/fa-solid-900.woff2",
 	"img/style.scss" => "img/sl-logo.png"
--- lib/boot.app.php
+++ lib/boot.app.php
@@ -11,7 +11,7 @@ use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Monolog\Processor\PsrLogMessageProcessor;
 
-define('_APP_VERSION', '2.13.0'); // auto-generated: 1578520361045
+define('_APP_VERSION', '2.13.0'); // auto-generated: 1578522502997
 define('_BASEDIR', realpath(__DIR__ . '/../'));
 
 $loader = require _BASEDIR . '/vendor/autoload.php';
